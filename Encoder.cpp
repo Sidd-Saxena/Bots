30980ace85493b14474557e47092bd882de36339
Siddharth Saxena <saxena.siddharth1992@gmail.com>
2015-04-19 20:34:53 -0400

    fixed using pololu encoders

--- Encoder.cpp
+++ Encoder.cpp
@@ -1,16 +1,15 @@
 #include "Encoder.h"
-#include "Motor.h"
 
-Encoder::Encoder(PinName pina,PinName pinb){
-//    _pin = 1;
+
+Encoder::Encoder(PinName pina,PinName pinb):_pina(pina),_pinb(pinb)
+{
+    
 }
  
-
 void Encoder::forward(int lidarReading) {
     //circumference of wheel  = 13.188cm
     //one encoder count = 2.638cm
-    encoderCount = (int)((float)lidarReading/2.638);
-    Motor.forward();
+    encoderCount = (int)((float)(lidarReading/2.638)+0.5);
     printf("Count = %d\n\r",encoderCount);
     startEncoding();
     return;
@@ -18,17 +17,16 @@ void Encoder::forward(int lidarReading) {
 
 void Encoder::startEncoding(){
     Serial pc(USBTX, USBRX); //Initalise PC serial comms
-    int currenta = 0, currentb = 0 ,previous = 0;
+    int currenta = 0, currentb = 0;
     while(encoderCount>0){
            currenta = _pina.read();
-           currenta = _pinb.read();
-            if(a==1 && b==1)
+           currentb = _pinb.read();
+            if(currenta==1 && currentb==1)
             {
                  encoderCount--;
                  printf("Count in encoding = %d\n\r",encoderCount);
             } 
     }
-    Motor.brake();
     pc.printf("Stopping bot");
     return;
 }
@@ -36,4 +34,4 @@ void Encoder::startEncoding(){
 
 float Encoder::read(){
     return(_pina.read() , _pinb.read());   
-}
+ }
