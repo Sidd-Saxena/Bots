45d144bb68ec8db6059414ffbb748dd8f13a6386
Siddharth Saxena <saxena.siddharth1992@gmail.com>
2015-04-19 20:35:32 -0400

    updated using pololu encoders

--- Encoder.h
+++ Encoder.h
@@ -1,6 +1,7 @@
-#ifndef MBED_ENCODER_H
-#define MBED_ENCODER_H
+#ifndef ENCODER_H
+#define ENCODER_H
  
+
 #include "mbed.h"
 
 class Encoder {
@@ -8,12 +9,10 @@ public:
     Encoder(PinName pina, PinName pinb);
     void forward(int);
     void startEncoding();
-    void stopBot();
     float read();
 private:  
     DigitalIn _pina;
-    DigitalIn _pinb
-    
+    DigitalIn _pinb; 
     int encoderCount;
 };
  
