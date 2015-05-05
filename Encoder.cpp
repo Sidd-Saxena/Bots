d5eebd86f7b93db3738ab87cacf936a3b82b2dce
Siddharth Saxena <saxena.siddharth1992@gmail.com>
2015-05-04 23:44:55 -0400

    Final Commit

--- Encoder.cpp
+++ Encoder.cpp
@@ -1,8 +1,11 @@
+/* ESE 519 Project : TURF
+Author : Siddharth Saxena, University of Pennsylvania
+This function controls the encoder and let them be read on digital pins of the mbed. It calculated the number of rotataion taken place. 
+5 times when both digital pins are 1 that complete one rotation this helps to moves the bot by desired distance.*/
+
 #include "Encoder.h"
  
 Encoder::Encoder(PinName eouta, PinName eoutb) : _eouta(eouta),_eoutb(eoutb){
-//    _eouta = 0;
-//    _eoutb = 0;
 }
  
 void Encoder::forward(int lidarReading) {
