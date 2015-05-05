49bac34c216b313884e2cb92e0ff76abd3d29c5c
Siddharth Saxena <saxena.siddharth1992@gmail.com>
2015-05-04 20:31:21 -0400

    few final  changes

--- main.cpp
+++ main.cpp
@@ -33,72 +33,80 @@ int main()
        // printf("%f",dis);
         //distance calculated to move forward
         r.forward(dis,0.1);
-        //ss << '<';
-         // Add the robot's position to the string
-        //int mytemp = TemperatureRead();
-//        mytemp=72;
-//        ss << r._x << " " << r._y << " " << r._th <<" "<<mytemp<<'\n'<<'\r';
-//        ss << '>'; 
-//        r.xbee.sendData(ss.str().c_str());
+        
+        //call function to read sorrounding temperature 
+        int mytemp = TemperatureRead();
+        
+         // Add the robot's position to the string      
+        ss << '<';
+        ss << r._x << " " << r._y << " " << r._th <<" "<<mytemp<<'\n'<<'\r';
+        ss << '>'; 
+        //send data about the co ordinates of the bot on xbee
+        r.xbee.sendData(ss.str().c_str());
         //counttime++;
     
         //ss << '<';
 //        ss <<path<<","<<"F";
 //        ss << '>';
         //r.xbee.sendData(ss.str().c_str());
-        //Logic for selecting left or right depending on servo scan 
         //st << 'f'<<dis;
         //rpath[flowcount] = st.str();
 //        flowcount++;
         dir = r.servo_scan();
+        //Logic for selecting left or right depending on servo scan 
         if (dir == "R")
         {
             r.right(0.1);
-            //ss << '<';
-//            ss <<path<<","<<"R";
-//            ss << '>'; 
-           // rpath[flowcount]="r0";
-//            flowcount++;
+            //keeping the record of the path travelled
+           ss << '<';
+           ss <<path<<","<<"R";
+           ss << '>'; 
+           rpath[flowcount]="r0";
+           //count the number of steps taken
+           flowcount++;
         }
         else if (dir == "L")
         {
             r.left(0.1);
-            //ss << '<';
-//            ss <<path<<","<<"L";
-//            ss << '>'; 
-         //  rpath[flowcount]="l0";
-//           flowcount++;
+            ss << '<';
+            ss <<path<<","<<"L";
+            ss << '>'; 
+            rpath[flowcount]="l0";
+            flowcount++;
         }
-       
-        }
-        //if (flowcount == 4)
-//        {
+    }
+    
+    //demonstating the going following the other bot functionality
+        if (flowcount == 4)
+        {
 //          printf("Entered reverse");
-//          reverse(4,rpath); 
-//          printf("Array: %s %s %s %s  \n \r" , rpath[0],rpath[1], rpath[2], rpath[3]);
-//        }
-//         for (int i=0; i<4; i++){
-//                        printf("looking forward");
-//                        if(rpath[i].at(0)=='f')
-//                        {
-//                         printf("Entered forward");
-//                         godista=rpath[i].substr(1,2);
-//                         godist=atoi(godista.c_str());
-//                         r.forward(godist,0.1);   
-//                        }
-//                      //  printf("looking right");
-//                         else if(rpath[i].at(0)=='r')
-//                        {
-//                            printf("Entered right");
-//                            r.right(0.1);
-//                        }
-//                        //printf("looking left");
-//                         else if(rpath[i].at(0)=='l')
-//                        {
-//                            printf("Entered left");
-//                            r.left(0.1);
-//                        }
-//                }         
+//enter the path travelled into an array
+          reverse(4,rpath); 
+//       printf("Array: %s %s %s %s  \n \r" , rpath[0],rpath[1], rpath[2], rpath[3]);
+        }
+        logic to follow the path travelled
+         for (int i=0; i<4; i++){
+                        //printf("looking forward");
+                        if(rpath[i].at(0)=='f')
+                        {
+                         printf("Entered forward");
+                         godista=rpath[i].substr(1,2);
+                         godist=atoi(godista.c_str());
+                         r.forward(godist,0.1);   
+                        }
+                      //  printf("looking right");
+                         else if(rpath[i].at(0)=='r')
+                        {
+                            printf("Entered right");
+                            r.right(0.1);
+                        }
+                        //printf("looking left");
+                         else if(rpath[i].at(0)=='l')
+                        {
+                            printf("Entered left");
+                            r.left(0.1);
+                        }
+                }         
     }
 
 /* Convert a data sample into a string to send over the xbee. */
@@ -116,7 +124,7 @@ const char * dataToString(DataSample ds)
     return ss.str().c_str();
 }
 
-//Multithreading
+//Multithreading to allow reading of LIDAR even when it's travelling
 
 void checkLidarThread(void const *args) {
     int countIf = 0, countElse = 0;
@@ -136,40 +144,43 @@ void checkLidarThread(void const *args) {
     }
 }
 
-//void reverse(int size, string* rpath){
-//    int start=0;
-//    int size_minus1=size-1;
-//    //reverse order
-//    string temp;
-//    while(start < size_minus1)
-//    {
-//    temp = rpath[start];   
-//    rpath[start] = rpath[size_minus1];
-//    rpath[size_minus1] = temp;
-//    start++;
-//    size_minus1--;
-//    }   
-//    //pc.printf("Fliiped Array: %s %s %s %s  \n \r" , rpath[0],rpath[1], rpath[2], rpath[3]);
-//    //change r to l and l to r
-//    for (int i=0; i<4; i++)
-//    {
-//    if (rpath[i].at(0)=='r')
-//        {
-//        rpath[i]='l'+rpath[i].erase(0,1);
-//        }
-//    else if (rpath[i].at(0)=='l')
-//        {
-//        rpath[i]='r'+rpath[i].erase(0,1);
-//        }
-//    }
-//    //pc.printf("Switched: %s %s %s %s  \n \r" , rpath[0],rpath[1], rpath[2], rpath[3]);   
-//       
-//                }
-//
-//int TemperatureRead()
-//{
-//          float sensorReading = Temp.read(); //read temperature from the sensor
-//          sensorReading=(((sensorReading*5000)/1024)-500)/10;
-//          int degreeF = ((sensorReading*9)/5)+32;
-//          return sensorReading;
-//}
+
+//logic to travel back to the battery station
+void reverse(int size, string* rpath){
+    int start=0;
+    int size_minus1=size-1;
+    //reverse order
+    string temp;
+    while(start < size_minus1)
+    {
+    temp = rpath[start];   
+    rpath[start] = rpath[size_minus1];
+    rpath[size_minus1] = temp;
+    start++;
+    size_minus1--;
+    }   
+    //pc.printf("Fliiped Array: %s %s %s %s  \n \r" , rpath[0],rpath[1], rpath[2], rpath[3]);
+    //change r to l and l to r
+    for (int i=0; i<4; i++)
+    {
+    if (rpath[i].at(0)=='r')
+        {
+        rpath[i]='l'+rpath[i].erase(0,1);
+        }
+    else if (rpath[i].at(0)=='l')
+        {
+        rpath[i]='r'+rpath[i].erase(0,1);
+        }
+    }
+    //pc.printf("Switched: %s %s %s %s  \n \r" , rpath[0],rpath[1], rpath[2], rpath[3]);   
+       
+}
+
+//function to read tempertaure (proof of idea about collecting senosr data)
+int TemperatureRead()
+{
+          float sensorReading = Temp.read(); //read temperature from the sensor
+          sensorReading=(((sensorReading*5000)/1024)-500)/10;
+          int degreeF = ((sensorReading*9)/5)+32;
+          return sensorReading;
+}
